a918d0e84df2a43cd712a39bc801ab0c3bd2e96e
p1xel <dima.neumoin1234@gmail.com>
2022-04-17 21:41:59 +0300

    add mysql database

--- askme/settings.py
+++ askme/settings.py
@@ -79,6 +79,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+
+    'mysql': {
+        'NAME': '',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': '',
+        'PASSWORD': ''
     }
 }
 
